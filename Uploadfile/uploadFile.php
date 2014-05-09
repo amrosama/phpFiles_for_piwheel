14a03fbb2bf4dcf17c25c890277ceea9bdb8c5eb
IbrahimAmin <ibrahimtorbany.dev@outlook.com>
2014-05-09 20:15:27 +0200

    uploadFile.php | New Changes
    Now messages are more meaningful, directory/file checks are more
    accurate, duplicate file names are not allowed ensuring no file will get
    overwritten.

--- Uploadfile/uploadFile.php
+++ Uploadfile/uploadFile.php
@@ -1,12 +1,30 @@
 <?php
+
 if ($_FILES["File"]["error"] > 0) {
-  echo false;
-} else {
-  if (!file_exists('/'.$_POST["Path"].'/')){
-      mkdir($_POST["Path"], 0700, true);
-      echo "path created";
-      move_uploaded_file($_FILES["File"]["tmp_name"], $_POST["Path"] . '/' . $_FILES["File"]["name"]);
-      echo "done";
-  }
+    if ($_FILES["File"]["error"] == 1) {
+        echo "ex_max"; //Exceeded maximum file size.
+    }
+    elseif ($_FILES["File"]["error"] == 7) {
+        echo "ex_dsk"; //Not enough space on disk.
+    }
+} 
+else {
+    $filepath = $_POST["Path"];
+    if (!is_dir($filepath)){ //Directory does not exists.
+        mkdir($_POST["Path"], 0777, true);
+        move_uploaded_file($_FILES["File"]["tmp_name"], $_POST["Path"].'/'.$_FILES["File"]["name"]);
+        echo "true"; //Directory created & File successfuly uploaded.
+    }
+    else { //Directory exists.
+        $filename = $_POST["Path"].'/'.$_FILES["File"]["name"];
+        if(!file_exists($filename)) { //File does not exist.
+            move_uploaded_file($_FILES["File"]["tmp_name"], $_POST["Path"].'/'.$_FILES["File"]["name"]);
+            echo "true"; //File successfuly uploaded.
+        }
+        else {
+            echo "ex_dup"; //Duplicate file names.
+        }
+    }
 }
+
 ?>
