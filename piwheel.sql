477ab398b13a1a45cf51e9aa0983e1ad587524c4
HebaKamel <hebakamel151@yahoo.com>
2014-06-28 15:23:58 -0700

    add column for Instructor_Rigisteration task in sql file

--- piwheel.sql
+++ piwheel.sql
@@ -705,6 +705,11 @@ CREATE TABLE IF NOT EXISTS `User` (
   `language` varchar(100) COLLATE utf8_bin NOT NULL,
   `IsLoggedIn` tinyint(1) NOT NULL,
   `tmp_email` varchar(255) COLLATE utf8_bin NOT NULL,
+  `CurrentPosition` varchar(255) COLLATE utf8_bin NOT NULL,
+  `Bio` varchar(255) COLLATE utf8_bin NOT NULL,
+  `certification` varchar(255) COLLATE utf8_bin NOT NULL,
+  `accreditedby` varchar(255) COLLATE utf8_bin NOT NULL,
+  `verificationLink` varchar(255) COLLATE utf8_bin NOT NULL,
   PRIMARY KEY (`ID`),
   UNIQUE KEY `Username` (`Username`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=20 ;
