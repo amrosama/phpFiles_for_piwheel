da0fc130a5caf192a1d61d35ac378ff9ccb1c956
HebaKamel <hebakamel151@yahoo.com>
2014-07-22 23:19:28 -0700

    instructor_registration
    edits

--- Instructor_Rigisteration/Instructor_Rigisteration.php
+++ Instructor_Rigisteration/Instructor_Rigisteration.php
@@ -8,7 +8,7 @@ if(isset ($_POST['Prefix']) && isset ($_POST['FullName']) &&  isset ($_POST['Cur
     $email = $_POST['Email'];
     $Password = $_POST['Password'];
     $Certification = $_POST['Certification'];
-    $Accreditedby = $_POST['Accreditedby'];
+    $Accreditedby = $_POST['Accreditedby']; 
     $VerificationLink = $_POST['VerificationLink'];
     $Username = $_POST['Email'];
     $RoleID = 3;
@@ -17,30 +17,31 @@ if(isset ($_POST['Prefix']) && isset ($_POST['FullName']) &&  isset ($_POST['Cur
     $VerificationLink = explode(",", $VerificationLink);
 }
 
-if(isset ($_FILES['Image'])){
-        if ($_FILES["Image"]["error"] > 0) {
+if(isset ($_FILES['image'])){
+        if ($_FILES["image"]["error"] > 0) {
            echo "Apologies, an error has occurred. ";
-           echo "Error Code: " . $_FILES["Image"]["error"];
+           echo "Error Code: " . $_FILES["image"]["error"];
         }
         else{
-           move_uploaded_file($_FILES["Image"]["tmp_name"],
-                              "images/" . $_FILES["Image"]["name"]);
+           move_uploaded_file($_FILES["image"]["tmp_name"],
+                              "images/" . $_FILES["image"]["name"]);
+//           echo "Saved";
         }
     }else{
         echo "else";
     }
 
-$con = mysqli_connect("PiWheel123.db.10962756.hostedresource.com","PiWheel123","P@ssw0rd90906","PiWheel123");
-//$con = mysqli_connect("localhost","root","","piwheel");
+//$con = mysqli_connect("PiWheel123.db.10962756.hostedresource.com","PiWheel123","P@ssw0rd90906","PiWheel123");
+$con = mysqli_connect("localhost","root","","piwheel");
 // Check connection
 if (mysqli_connect_errno()) {
   echo "Failed to connect to MySQL: " . mysqli_connect_error();
 }else{
       $command = "insert into user(Prefix,Username,Fullname,CurrentPosition,Image,Bio,tmp_email,Password,RoleID)" .
-                 " values('". $Prefix . "','" .$Username . "','" . $FullName . "','" . $CurrentPosition . "','"  .  $_FILES["Image"]["name"] . "','"
+                 " values('". $Prefix . "','" .$Username . "','" . $FullName . "','" . $CurrentPosition . "','"  .  $_FILES["image"]["name"] . "','"
                          .  $Bio ."','" 
                         .  $email ."','"  . $Password ."','"  . $RoleID ."'" . ")";
-    //echo $command . "<br/>";
+    echo $command . "<br/>";
     if(mysqli_query($con, $command)){
        echo "inserted"; 
        $commandText = "SELECT * FROM user ORDER BY id DESC LIMIT 1";
@@ -59,16 +60,16 @@ if (mysqli_connect_errno()) {
         }
         $command = "insert into certification(userID,certName,acreditedby,verificationLink)" .
                  " values('". $userID . "','" .$Certification[$i] . "','" . $Accreditedby[$i] . "','" . $VerificationLink[$i] ."'" . ")";
-        //echo $command . "<br/>";
+        echo $command . "<br/>";
         if(mysqli_query($con, $command)){
            echo " inserted"; 
         }else{
-            echo "sorry, not inserted";
+            echo "sorry,cer not inserted";
         }
         //echo $Certification[$i] . "<br/>";
     }
     }else{
-        echo "sorry, not inserted";
+        echo "sorry,user not inserted";
     }
     //get user id from user table
     
